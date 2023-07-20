b6bd94cba896fa2a6f11dcf2eeaae57e67efa9ac
Caio Costa <107070158+CaioCosta182@users.noreply.github.com>
2023-07-19 21:20:37 -0300

    Update ListaMatrizE17.java

--- Lista Matriz revisão/ListaMatriz E17/src/listamatriz/e17/ListaMatrizE17.java
+++ Lista Matriz revisão/ListaMatriz E17/src/listamatriz/e17/ListaMatrizE17.java
@@ -1,57 +1,69 @@
 /*
-Faça um programa capaz de corrigir provas de múltipla escolha. Cada prova terá 10 questões,
-valendo 1 ponto cada uma. O programa deve inicialmente ler o gabarito da prova. Ele será
-constituído de um conjunto de dez caracteres representando a resposta para cada uma das
-questões. Cada questão pode ter uma das seguintes respostas: 'a', 'b', 'c', 'd' ou 'e'.
-Após a leitura do gabarito, o programa deverá ler as respostas dos alunos. Para cada aluno
-deverá ser lido o seu número de matrícula e suas respostas. O programa deverá calcular a nota
-do aluno e imprimi-la de acordo com o gabarito. A leitura termina com um código de matrícula
-negativo. Ao final, o programa deverá imprimir a percentagem de aprovação, sabendo-se que a
-nota mínima é 5.
+Faça um programa em Java para corrigir uma prova com 10 questões de múltipla escolha( a, b, c, d ou e) de uma turma com 3 alunos. 
+Cada questão vale 1 ponto. Para proceder com a correção, o programa deve ler o gabarito. 
+Em seguida, ler as respostas dos alunos, e comparar essas respostas com o gabarito. 
+A cada acerto, o aluno ganha 1 ponto. Ao final o programa deve calcular e escrever para cada aluno, a sua nota (0 a 10). 
+E apresentar o número de notas que ficaram acima da média para aprovação, assumindo que essa média é 6.
+
+Entrada
+
+A entrada é composta de 4 sequências de 10 caracteres (composto por letras minúsculas, de a - e), 
+onde a primeira sequência corresponde ao gabarito e as 3 sequências seguintes correspondem às respostas dos 3 alunos.
+
  */
 package listamatriz.e17;
 import java.util.Scanner;
 public class ListaMatrizE17 {
-    public static void main(String[] args) {
-       
-        Scanner teclado = new Scanner(System.in);
-        String gabarito[] = new String[10], resposta[] = new String[10];
-        int notas[] = new int[10];
-        int qAlunos = 0, qAprovados = 0, nota = 0, j =0;
+        public static void main(String[] args) {
+            Scanner teclado = new Scanner(System.in);
+         
+           
+            //para ler as questões do gabarito
+         System.out.println("Informe um caractere referente a resposta do gabarito: ");
+        char gabarito[] = new char[10];
+        String teste = teclado.nextLine();
+        for (int i = 0; i < 10; i++) {
+            gabarito[i] = teste.charAt(i);
 
-        System.out.println("Digite o gabarito da prova: (10 caracteres)");
-       for(int i = 0; i < 10; i++){
-        gabarito[i] = teclado.next();
-       }
-        while(true){
-            System.out.print("Digite o número da matrícula do aluno, ou um número negativo para finalizar o programa:");
-            int matricula = teclado.nextInt();
-                if(matricula < 0){
-                    break;
-                }
-                qAlunos++;
-            System.out.print("Digite as respostas do aluno" + matricula + ":(10 caracteres)");
-            for(int i = 0; i < 10; i++){
-            resposta[i] = teclado.next();
-            }
-            
-            for(int i  = 0; i < 10; i++){
-                if((resposta[i]).equals(gabarito[i])){
-                nota++;
-                notas[j] = nota;
-                j++;
-                }
-            }
-            System.out.println("Matricula: " + matricula);
-            System.out.println("Nota: " + nota);
-            
-            if(nota >= 5){
-                qAprovados++;
-            }
-            System.out.println();
         }
-        double percentAprovados = qAprovados / qAlunos * 100;
-        System.out.println("Percentagem de aprovação: " + percentAprovados + "%");
+        
+        // Lendo as respostas dos alunos
+        System.out.println("Informe as notas dos alunos : ");
+        char[][] questAlunos = new char[3][10];
+        String sequencia;
+        for (int i = 0; i < 3; i++) {
+            sequencia = teclado.nextLine();
+            for (int j = 0; j < 10; j++) {
+                questAlunos[i][j] = sequencia.charAt(j);
+            }
+        }
+        
+        // Corrigindo as respostas e calculando as notas
+        int[] notas = new int[3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 10; j++) {
+                if (questAlunos[i][j] == gabarito[j]) {
+                    notas[i]++;
+                }
+            }
+        }
+        
+        // Imprimindo as notas dos alunos
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Nota do Aluno" + (i + 1) + ": " + notas[i]);
+        }
+        
+        // Calculando o número de notas acima da média
+        int notasAcimaMedia = 0;
+        for (int i = 0; i < 3; i++) {
+            if (notas[i] >= 6) {
+                notasAcimaMedia++;
+            }
+        }
+        
+        // Imprimindo o número de notas acima da média
+        System.out.println("Notas acima da média: " + notasAcimaMedia);
+
     
     }
 }
